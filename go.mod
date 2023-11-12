09e604940ddc48525a8092372dd1809c9de6900e
David Baker <dpbaker1298@gmail.com>
2023-11-11 22:11:02 -0500

    Add dependency and test case for process termination

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Dbaker1298/goci
 
 go 1.21.3
+
+require github.com/go-errors/errors v1.5.1 // indirect
--- main_test.go
+++ main_test.go
@@ -4,10 +4,15 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 	"testing"
 	"time"
+
+	"github.com/go-errors/errors"
 )
 
 func TestRun(t *testing.T) {
@@ -107,4 +112,64 @@ func TestHelperProcess(t *testing.T) {
 	}
 
 	os.Exit(1)
+}
+
+func TestRunKill(t *testing.T) {
+	testCases := []struct {
+		name   string
+		proj   string
+		sig    syscall.Signal
+		expErr error
+	}{
+		{"SIGINT", "./testdata/tool/", syscall.SIGINT, ErrSignal},
+		{"SIGTERM", "./testdata/tool/", syscall.SIGTERM, ErrSignal},
+		{"SIGQUIT", "./testdata/tool/", syscall.SIGQUIT, nil},
+	}
+
+	// RunKill Test Execution
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			command = mockCmdTimeout
+			errCh := make(chan error)
+			ignSigCh := make(chan os.Signal, 1)
+			expSigCh := make(chan os.Signal, 1)
+
+			signal.Notify(ignSigCh, syscall.SIGQUIT)
+			defer signal.Stop(ignSigCh)
+			signal.Notify(expSigCh, tc.sig)
+			defer signal.Stop(expSigCh)
+
+			go func() {
+				errCh <- run(tc.proj, ioutil.Discard)
+			}()
+
+			go func() {
+				time.Sleep(2 * time.Second)
+				syscall.Kill(syscall.Getpid(), tc.sig)
+			}()
+
+			// select error
+			select {
+			case err := <-errCh:
+				if err == nil {
+					t.Errorf("Expected error: %q. Got 'nil' instead.", tc.expErr)
+					return
+				}
+
+				if !errors.Is(err, tc.expErr) {
+					t.Errorf("Expected error: %q. Got %q instead.", tc.expErr, err)
+				}
+				// select signal
+				select {
+				case rec := <-expSigCh:
+					if rec != tc.sig {
+						t.Errorf("Expected signal: %q. Got %q instead.", tc.sig, rec)
+					}
+				default:
+					t.Errorf("Signal not received")
+				}
+			case <-ignSigCh:
+			}
+		})
+	}
 }
